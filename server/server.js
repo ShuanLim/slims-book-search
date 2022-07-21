1c8eb61d2d56774ed023e3d0af916d4d50636d04
s.limguy <shuanmlim@icloud.com>
2022-07-21 00:01:28 -0700

    fixed async issues

--- server/server.js
+++ server/server.js
@@ -6,29 +6,36 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./schemas');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-server.applyMiddleware({ app });
+const startServer = async () => {
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-// app.use(routes);
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(` Now listening on localhost:${PORT}`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
-});
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // app.use(routes);
+
+  db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(` Now listening on localhost:${PORT}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+startServer();
